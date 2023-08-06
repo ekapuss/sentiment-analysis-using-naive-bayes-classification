6510b23b788d5ab5e9d4bfadf68b1caffd739c7c
ekapuss <ekapuspita404@gmail.com>
2023-08-06 23:17:20 +0800

    sentiment commit

---
+++ Env/Lib/site-packages/psycopg_binary/__init__.py
@@ -0,0 +1,47 @@
+"""
+psycopg -- PostgreSQL database adapter for Python -- C optimization package
+"""
+
+
+# start delvewheel patch
+def _delvewheel_init_patch_0_0_22():
+    import os
+    import sys
+    libs_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir, 'psycopg_binary.libs'))
+    if sys.version_info[:2] >= (3, 8):
+        conda_workaround = os.path.exists(os.path.join(sys.base_prefix, 'conda-meta')) and (sys.version_info[:3] < (3, 8, 13) or (3, 9, 0) <= sys.version_info[:3] < (3, 9, 9))
+        if conda_workaround:
+            # backup the state of the environment variable CONDA_DLL_SEARCH_MODIFICATION_ENABLE
+            conda_dll_search_modification_enable = os.environ.get('CONDA_DLL_SEARCH_MODIFICATION_ENABLE')
+            os.environ['CONDA_DLL_SEARCH_MODIFICATION_ENABLE'] = '1'
+        os.add_dll_directory(libs_dir)
+        if conda_workaround:
+            # restore the state of the environment variable CONDA_DLL_SEARCH_MODIFICATION_ENABLE
+            if conda_dll_search_modification_enable is None:
+                os.environ.pop('CONDA_DLL_SEARCH_MODIFICATION_ENABLE', None)
+            else:
+                os.environ['CONDA_DLL_SEARCH_MODIFICATION_ENABLE'] = conda_dll_search_modification_enable
+    else:
+        from ctypes import WinDLL
+        with open(os.path.join(libs_dir, '.load-order-psycopg_binary-3.0.15')) as file:
+            load_order = file.read().split()
+        for lib in load_order:
+            WinDLL(os.path.join(libs_dir, lib))
+
+
+_delvewheel_init_patch_0_0_22()
+del _delvewheel_init_patch_0_0_22
+# end delvewheel patch
+
+
+
+# Copyright (C) 2020 The Psycopg Team
+
+import sys
+
+# This package shouldn't be imported before psycopg itself, or weird things
+# will happen
+if "psycopg" not in sys.modules:
+    raise ImportError("the psycopg package should be imported before psycopg_binary")
+
+from .version import __version__ as __version__  # noqa
